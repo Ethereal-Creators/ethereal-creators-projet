7cab89e62060b2c3d26dd003e7c37c157d28056b
SparkyPX <pierrelucproulx@gmail.com>
2024-12-02 15:11:27 -0500

    FIXED probleeem that maik wanted to fix cuz it will make him pass his semester

--- docsify.js
+++ docsify.js
@@ -3,11 +3,9 @@ window.$docsify = {
     repo: 'https://github.com/Ethereal-Creators/ethereal-creators-projet',
     auto2top: true,
     themeColor: '#3F51B5',
-    topMargin: 90,
     loadSidebar: false,
     autoHeader: true,
     nativeEmoji: true,
-    executeScript: true,
     fallbackLanguages: ['fr', 'en'],
     maxLevel: 3,
     subMaxLevel: 3,
@@ -27,19 +25,4 @@ window.$docsify = {
         }]
     ],
     theme: 'dark', // Enable dark mode
-    plugins: [
-        function (hook, vm) {
-            hook.doneEach(() => {
-                // Lightbox plugin for images
-                import('https://gllmar.github.io/docsify-lightbox/docsify-lightbox.js').then(
-                    module => {
-                        const images = document.querySelectorAll('img');
-                        const lightbox = new module.default();
-                        lightbox.initialize(images);
-                    }).catch(err => console.error('Failed to load Lightbox plugin', err));
-            });
-        },
-
-       
-    ],
 };
